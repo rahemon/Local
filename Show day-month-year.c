c058dda2ac6db41c928067d0ed5e6cf5920a92f4
MD.RAHEMON SARKER <121973161+rahemon@users.noreply.github.com>
2023-01-09 17:00:38 +0600

    Add files via upload

---
+++ Show day-month-year.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int day,month,year;
+    printf("Enter day:");
+    scanf("%d",&day);
+     printf("Enter month:");
+      scanf("%d",&month);
+      printf("Enter year:");
+      scanf("%d",&year);
+      printf("Todays date:%d-%d-%d",day,month,year);
+
+
+return 0;
+}
