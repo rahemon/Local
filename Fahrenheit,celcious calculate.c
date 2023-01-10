c4cc1fb7a302876f1eb91c171485d09be3e4fa82
MD.RAHEMON SARKER <121973161+rahemon@users.noreply.github.com>
2023-01-10 21:05:33 +0600

    Add files via upload

---
+++ Fahrenheit,celcious calculate.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    float c,f;
+    printf("Enter Celcious:");
+    scanf("%f",&c);
+    printf("Fahrenheit=%f",c*9+160/5);
+    printf("\nEnter Fahrenheite:");
+    scanf("%f",&f);
+    printf("Calcious=%f",5*f-160/9);
+
+return 0;
+}
